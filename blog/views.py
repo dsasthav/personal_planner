e9ebdb5c0c7d0d8623bbfecc31d9954a28d1caa5
Derek Sasthav <dereksasthav@squareup.com>
2023-12-01 13:48:35 -0500

    get rid of blog-home url

--- blog/views.py
+++ blog/views.py
@@ -11,17 +11,8 @@ from django.views.generic import (
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
-
-
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
 
