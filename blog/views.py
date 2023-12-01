2883b1fcd53418243fdd66c1f0ea957baa10d2dc
Derek Sasthav <dereksasthav@squareup.com>
2023-11-30 21:39:49 -0500

    adding model and migration

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from models import Post
+from .models import Post
 
-# Create your views here.
+
 def home(request):
     context = {
         'posts': Post.objects.all()
